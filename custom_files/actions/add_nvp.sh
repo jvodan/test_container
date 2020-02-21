83b5553ae9f321ba8264c366da957dc03c556698
Mark Ratjens <mark@ratjens.com>
2020-02-21 12:42:37 +1100

    quialify database-oriented substitution parameters with dependent name
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- custom_files/actions/add_nvp.sh
+++ custom_files/actions/add_nvp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 /home/engines/functions/params_to_env.sh
 parms_to_env
-echo \"INSERT INTO action_tests (name, value) VALUES ('$name', '$value'); \"| mysql -h ^^mysql_host^^ -u ^^dbuser^^  --password=^^dbpasswd^^  ^^dbname^^
+echo \"INSERT INTO action_tests (name, value) VALUES ('$name', '$value'); \"| mysql -h ^^db_2.host^^ -u ^^db_2.username^^  --password=^^db_2.password^^  db_2
--- custom_files/actions/list_test.sh
+++ custom_files/actions/list_test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo \"select * from action_tests \" | mysql -h ^^mysql_host^^ -u ^^dbuser^^  --password=^^dbpasswd^^ ^^dbname^^
+echo \"select * from action_tests \" | mysql -h ^^db_2.host^^ -u ^^db_2.username^^  --password=^^db_2.password^^ db_2
